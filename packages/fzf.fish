8edec52144eed5e2cca8eabd03433796b4cfc4e8
Patrick <patrickf3139@gmail.com>
2020-10-27 17:01:36 -0700

    Update packages/fzf.fish
    Co-authored-by: Pablo Aguiar <scorphus@gmail.com>

--- packages/fzf.fish
+++ packages/fzf.fish
@@ -1,4 +1,4 @@
 type = plugin
 repository = https://github.com/patrickf3139/fzf.fish
 maintainer = patrickf3139
-description = Integrate fzf into fish with mnemonic keybindings. A newer, more feature-rich, more maintained alternative `jethrokuan/fzf`.
+description = Integrate fzf into fish with mnemonic keybindings. A newer, more feature-rich, more maintained alternative to `jethrokuan/fzf`.
